805c0abcfa30cb3bdbede83d111463042f4b80d9
Pedro Tortello <ptortello@hotmail.com>
2022-01-03 16:48:08 -0300

    noopener added

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -11,13 +11,13 @@ function Header() {
       <S.Title>Pedro Tortello</S.Title>
       <S.SubTitle>Desenvolvedor front-end</S.SubTitle>
       <S.Social>
-        <S.Link to={{pathname: "https://www.linkedin.com/in/pedrotortello/"}} target="_blank">
+        <S.Link to={{pathname: "https://www.linkedin.com/in/pedrotortello/"}} target="_blank" rel="noopener">
           <S.Image src={linkedin} alt='LinkedIn' />
         </S.Link>
-        <S.Link to={{pathname: "https://github.com/ptortello/"}} target="_blank">
+        <S.Link to={{pathname: "https://github.com/ptortello/"}} target="_blank" rel="noopener">
           <S.Image src={github} alt='GitHub' />
         </S.Link>
-        <S.Link to={{pathname: "https://api.whatsapp.com/send?phone=5515991049183/"}} target="_blank">
+        <S.Link to={{pathname: "https://api.whatsapp.com/send?phone=5515991049183/"}} target="_blank" rel="noopener">
           <S.Image src={whatsapp} alt='WhatsApp' />
         </S.Link>
       </S.Social>
